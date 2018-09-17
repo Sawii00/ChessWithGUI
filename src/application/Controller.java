3c4ab10577e53672a264ec0cc82054863a01e1fa
Sawii00 <s.nasturzio@libero.it>
2018-09-17 09:38:16 +0200

    Trying to merge

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -126,11 +126,17 @@ public class Controller {
 		Menu help = menu.getMenus().get(2);
 
 		// @TODO we have to set the reset button
-		MenuItem reset = game.getItems().get(0);
-		MenuItem close = game.getItems().get(1);
+		MenuItem importGame = game.getItems().get(0);
+		MenuItem reset = game.getItems().get(1);
+		MenuItem close = game.getItems().get(2);
 		CheckMenuItem host = (CheckMenuItem) multiplayer.getItems().get(0);
 		CheckMenuItem connect = (CheckMenuItem) multiplayer.getItems().get(1);
 		MenuItem about = help.getItems().get(0);
+		importGame.setOnAction(e -> {
+
+			new ImportBox("Import", 250, 250);
+
+		});
 
 		reset.setOnAction(e -> {
 			if (host.isSelected() || connect.isSelected()) {
